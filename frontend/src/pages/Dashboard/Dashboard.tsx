12d4cc7ba1295fd3bc2e35b52a3a61fb677feb87
Amber Holland <68969731+amshholland@users.noreply.github.com>
2025-12-10 16:30:45 +0000

    refactor: uncomment Transactions component in Dashboard

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Dashboard.css'
 import PlaidLink from '../../components/PlaidLink/PlaidLink.tsx'
+import Transactions from '../Transactions/Transactions.tsx'
 
 const Dashboard: React.FC = () => {
   return (
     <div className="dashboard">
       <PlaidLink />
-      {/* <Transactions /> */}
+      <Transactions />
     </div>
   )
-};
+}
 
 export default Dashboard;
